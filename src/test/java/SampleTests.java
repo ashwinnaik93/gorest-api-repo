3b7cb8b77c98681d6dddfc848fa8df1935e4b74a
Ashwin Naik <ashwinnaik94@gmail.com>
2023-01-20 19:22:58 +0530

    Intial Commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+
+    @Test
+    public void shouldTestSomething() {
+        Assert.assertEquals(1, 1);
+    }
+}
